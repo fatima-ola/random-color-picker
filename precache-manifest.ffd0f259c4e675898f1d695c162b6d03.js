ba5e71195fba86b579bf6ef03f203432e0a326e4
fatima-ola <fatimaolasunkanmi01@gmail.com>
2020-06-19 15:52:19 +0100

    Updates

---
+++ precache-manifest.ffd0f259c4e675898f1d695c162b6d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d44e918cffc96b6fccb50c8d9228f23",
+    "url": "/random-color-picker/index.html"
+  },
+  {
+    "revision": "7bf40aaf2e2021747cc1",
+    "url": "/random-color-picker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ccfdbf225833098d197b",
+    "url": "/random-color-picker/static/js/2.8f081367.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-color-picker/static/js/2.8f081367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf40aaf2e2021747cc1",
+    "url": "/random-color-picker/static/js/main.c92232b0.chunk.js"
+  },
+  {
+    "revision": "04597991594c08765aa9",
+    "url": "/random-color-picker/static/js/runtime-main.60c6fe63.js"
+  }
+]);
